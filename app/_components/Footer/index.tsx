ad430153a06abde81cc7b98ccb65eb9da6b24ea8
su <msnr7323@gmail.com>
2025-05-06 19:06:33 +0900

    4章まで完了

--- app/_components/Footer/index.tsx
+++ app/_components/Footer/index.tsx
@@ -1,23 +1,23 @@
+import Link from "next/link";
 import styles from "./index.module.css";
 
 export default function Header() {
-    return (
-        <footer className={styles.footer}>
-            <nav className={styles.nav}>
-                <ul className={styles.items}>
-                    <li className={styles.item}>
-                        <a href="/news">ニュース</a>
-                    </li>
-                    <li className={styles.item}>
-                        <a href="/members">メンバーズ</a>
-                    </li>
-                    <li className={styles.item}>
-                        <a href="/contact">お問い合わせ</a>
-                    </li>
-
-                </ul>
-            </nav>
-            <p className={styles.cr}>&copy SIMPLE. All Rights Reserved</p>
-        </footer>
-    );
+  return (
+    <footer className={styles.footer}>
+      <nav className={styles.nav}>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <Link href="/news">ニュース</Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/members">メンバー</Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/contact">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
+      <p className={styles.cr}>&copy SIMPLE. All Rights Reserved</p>
+    </footer>
+  );
 }
